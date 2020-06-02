c282ec698b0a15e4cc322a3f56d23d8351bb6716
Rafael Pereira Da Trindade <rafael.da.trindade@avanade.com>
2020-06-02 12:42:37 -0300

    atualizando credenciais

--- config/firebase.js
+++ config/firebase.js
@@ -4,13 +4,14 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2FdxqvTqAt1aPstk4EQv0v3W6J2tFwtk",
-  authDomain: "anonibus-23bbf.firebaseapp.com",
-  databaseURL: "https://anonibus-23bbf.firebaseio.com",
-  projectId: "anonibus-23bbf",
-  storageBucket: "anonibus-23bbf.appspot.com",
-  messagingSenderId: "72805023943",
-  appId: "1:72805023943:web:e19afaafa38be93ea2bee2"
+  apiKey: "AIzaSyDlYDxvBuSCE38kfsVnGgzUTaMaWaKoXzk",
+  authDomain: "anonibus-3842b.firebaseapp.com",
+  databaseURL: "https://anonibus-3842b.firebaseio.com",
+  projectId: "anonibus-3842b",
+  storageBucket: "anonibus-3842b.appspot.com",
+  messagingSenderId: "67355074230",
+  appId: "1:67355074230:web:64af46b51229e2dc0f9017",
+  measurementId: "G-N8SZEPQ53L"
 };
 
 export default !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
--- services/axios.js
+++ services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://us-central1-anonibus-23bbf.cloudfunctions.net',
+  baseURL: 'https://us-central1-anonibus-3842b.cloudfunctions.net',
 });
 
 export default api;
